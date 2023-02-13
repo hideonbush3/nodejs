851ecb8daa891afc9ab0adb2fe4b485abea80c8b
hideonbush3 <ppoii0961@gmail.com>
2023-02-13 11:41:09 +0900

    data.go.kr - 미세먼지 출력 (xml), 등급

--- crwaling04.js
+++ crwaling04.js
@@ -4,6 +4,7 @@
 
 // 사용할 패키지 가져오기 : require('패키지명')
 const axios = require("axios"); // Ajax 라이브러리
+const { XMLParser } = require("fast-xml-parser"); // xml 처리기 라이브러리
 
 async function main() {
   // 비동기 지원 함수 정의
@@ -11,38 +12,49 @@ async function main() {
   // https://www.data.go.kr/iim/api/selectAPIAcountView.do에서
   // 미리보기 눌렀을때 질의문자열('?'로 시작) 앞까지의 url 복사해서 URL에 할당
   // https 아니고 http로 써야함
+
+  // 인증 vs 인가
   const URL =
     "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+
+  // 요청변수(Request Parameter) 설정
   const params = {
     // serviecekey는 일반인증키(Decoding)
     servicekey:
       "B3vwYzTiX5fhDvELyU0et7PBBK38hsVRfmdjyLb8AiqTnlWKhqCGGgEiAFu7Gh1ulUPTOs/KHe6qsnzbv/CJow==",
-    returnType: "json",
+    returnType: "xml",
     numOfRows: 1000,
     sidoName: "서울",
     ver: 1.3,
   };
+
   const headers = {
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78",
   };
 
   // url을 axios로 비동기적으로 접속해서 대기오염 정보를 받아옴
-  const json = await axios.get(URL, {
+  const xml = await axios.get(URL, {
     params: params,
     headers: headers,
   }); // 서버 요청시 User-Agent 헤더 사용,  HTTP 응답 객체로 반환
 
   // 받아온 데이터 확인
-  console.log(json.data);
+  // console.log(xml);
+  // console.log(xml.data);
 
-  // JSON으로 불러오기
-  let items = json.data["response"]["body"]["items"]; // response내의 body내의 items
-  // console.log(items);
+  // XML 을 JSON 으로 변환
+  const parser = new XMLParser();
+  let json = parser.parse(xml.data);
+  // console.log(json);
+  // console.log(xml.data);
+  // console.log(xml);
 
+  let items = json["response"]["body"]["items"]; // response내의 body내의 items
+  // console.log(items);  // 출력해보면 items 객체 개부의 item이라는 배열 내부에 데이터들이 객체형태로 담겨있다
   // 미세먼지 정보 출력
   // pm25Value는 출력안됨! -> 파라미터에 버전 1.3으로 설정하니 됨
-  for (let item of items) {
+  for (let item of items["item"]) {
     console.log(
       item.sidoName,
       item.stationName,
