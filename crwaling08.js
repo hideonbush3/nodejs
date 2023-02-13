895ae275c8a6d13ab618d86bc6d33d6eee6228d4
hideonbush3 <ppoii0961@naver.com>
2023-02-13 18:07:45 +0900

    k-apt - 우리단지 검색

---
+++ crwaling08.js
@@ -0,0 +1,98 @@
+// 셀레니엄을 이용해서 k-apt.go.kr에서
+// 2023년 1월, 서울특별시, 강남구, 삼성동, 아이파크삼성 apt의
+// 지상/지하 주차장 수 추출
+
+const {
+  Builder,
+  Browser,
+  By,
+  Key,
+  until,
+  Select,
+} = require("selenium-webdriver");
+const cheerio = require("cheerio");
+
+async function main() {
+  const URL = "http://www.k-apt.go.kr/";
+
+  const chrome = await new Builder().forBrowser(Browser.CHROME).build();
+
+  try {
+    await chrome.get(URL);
+
+    // 우리단지 기본정보 버튼이 표시될때까지 5초 대기
+    await chrome.wait(until.elementLocated(By.css("ul.wp220 li a")));
+    await chrome.sleep(1000);
+
+    // 단지정보 버튼 클릭
+    // let menu = await chrome.findElement(
+    //     By.css("#nav > ul > li:nth-child(1) > a")
+    // );
+    let menu = await chrome.findElement(
+      By.xpath('//a[@title="단지정보"]') // '/a'는 모든 a태그란 뜻 '@'는 속성이라는 뜻
+    );
+    await chrome.actions().move({ origin: menu }).click().perform();
+    await chrome.sleep(500);
+
+    // 우리단지 기본정보 버튼 클릭
+    // const info = await chrome.findElement(By.css(".wp220 li:first-child a"));
+    let info = await chrome.findElement(
+      By.xpath('//a[@title="우리단지 기본정보"]')
+    );
+    await chrome.actions().move({ origin: info }).click().perform();
+    await chrome.sleep(500);
+
+    // ------------------------------
+    // 검색할 아파트를 변수로 선언
+    let syear = "2023년";
+    let smonth = "01월";
+    let sido = "서울특별시";
+    let gugun = "강남구";
+    let dong = "삼성동";
+    let apt = "아이파크삼성동";
+
+    // 검색년도 값 설정
+    let select = await chrome.findElement(By.name("searchYYYY"));
+    await new Select(select).selectByVisibleText(syear);
+    await sleep(300);
+
+    // 검색월 값 설정
+    select = await chrome.findElement(By.name("searchMM"));
+    await new Select(select).selectByVisibleText(smonth);
+    await sleep(300);
+
+    // 검색 시도 값 설정
+    select = await chrome.findElement(By.name("combo_SIDO"));
+    await new Select(select).selectByVisibleText(sido);
+    await sleep(300);
+
+    // 검색 구군 값 설정
+    select = await chrome.findElement(By.name("combo_SGG"));
+    await new Select(select).selectByVisibleText(gugun);
+    await sleep(300);
+
+    // 검색 동 값 설정
+    select = await chrome.findElement(By.name("combo_EMD"));
+    await new Select(select).selectByVisibleText(dong);
+    await sleep(300);
+
+    // 검색결과 출력 - 아파르명, 주소
+    let apts = await chrome.findElements(By.css('.aptS_rLName'));
+    for (let apt of apts){
+      console.log(await apt.getAttribute("textContent"));
+    }
+    let aptaddrs = await chrome.findElements(By.css('.aptS_rLAdd'));
+    for (let addr of aptaddrs){
+      console.log(await addr.getAttribute("textContent"));
+    }
+
+  } catch (ex) {
+    console.log(ex);
+  } finally {
+    await chrome.sleep(1000);
+    await chrome.quit();
+  }
+}
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+main();
